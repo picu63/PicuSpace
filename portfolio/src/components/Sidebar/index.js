e97ec84dfddefa1fbf129e1cc08fc0319278b29d
Piotr Olearczyk <piotr.olearczyk@medius.com>
2022-11-21 00:59:49 +0100

    sync - stuck with adding icons to sidebar from fontawesome

---
+++ portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,44 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-Logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <a href="https://www.linkedin.com/in/piotrolearczyk/">
+          {/* <FontAwesomeIcon icon="fa-brands fa-linkedin-in" /> */}
+          <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar
