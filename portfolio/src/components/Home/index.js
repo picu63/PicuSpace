ee27da120b384f4c61245de3cbf8ba10d89732ff
Piotr Olearczyk <piotr.olearczyk@medius.com>
2023-02-10 14:47:44 +0100

    sync

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-picu-transparent.svg'
+import PersonalImage from '../../assets/images/personal-image-ai-drawing.png'
 import './index.scss'
 
 const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
+        <img src={PersonalImage} alt="man in black" width="400px" />
         <h1>
-          Hi, <br /> I'm PI
+          Hi, <br /> I'm Piotr Olearczyk aka PI
           <img src={LogoTitle} alt="developer" />
-          software developer
+          &nbsp; software developer
         </h1>
         <h2>.NET Developer .......</h2>
         <Link to="/contact" className="flat-button">
