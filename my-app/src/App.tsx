dc29be19d6011103c001ad1632cb14fc7b019473
picu63 <picu63@gmail.com>
2023-04-05 00:41:39 +0200

    Added ready project to future adjustments

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 import Blog from './Blog/Blog';
+import Header from './components/Banner/Header';
+import ProfComponent from './components/Banner/ProfComponent';
+import Clients from './components/Clients/Clients';
+import Footer from './components/Footer/Footer';
+import Projects from './components/Projects/Projects';
+import Services from './components/Service/Services';
 
 function App() {
     console.log(`App version: ${process.env.REACT_APP_VERSION}`);
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>Hi, I'm Piotr Olearczyk and I'm software .NET developer</p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
-            <Blog />
+        // <div className='App'>
+        <div>
+            <Container>
+                <Banner>
+                    <Header />
+                    <ProfComponent />
+                </Banner>
+                <Services />
+                <LightColor>
+                    <Projects />
+                </LightColor>
+                <Clients />
+                <LightColor>
+                    <Footer />
+                </LightColor>
+            </Container>
+            {/* <LightColor>
+                <Blog />
+            </LightColor> */}
         </div>
     );
 }
 
 export default App;
+
+const Container = styled.div``;
+const Banner = styled.div`
+    background: linear-gradient(
+        159deg,
+        rgb(45, 45, 58) 0%,
+        rgb(43, 43, 53) 100%
+    );
+    height: 100vh;
+    @media (max-width: 640px) {
+        height: 100%;
+        padding-bottom: 2rem;
+    }
+`;
+
+const LightColor = styled.div`
+    background: linear-gradient(
+        159deg,
+        rgb(45, 45, 58) 0%,
+        rgb(43, 43, 53) 100%
+    );
+`;
